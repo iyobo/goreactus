57279bd3b3b68d37f267b3183d489e09630c841c
Iyobo <phil.eki@gmail.com>
2015-12-12 01:10:32 -0500

    Golang Rest API

---
+++ src/goreactus/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	_ "goreactus/docs"
+	_ "goreactus/routers"
+
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	if beego.RunMode == "dev" {
+		beego.DirectoryIndex = true
+		beego.StaticDir["/swagger"] = "swagger"
+	}
+	beego.Run()
+}
